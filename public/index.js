1754e25e083aa0f790fcc04be01c5660a997fb09
DanielCruzAr <daniel_cruzar@outlook.com>
2024-10-07 13:39:36 -0600

    fixed layout

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+const logo = document.querySelector('#logo');
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
@@ -7,4 +8,8 @@ burger.addEventListener('click', () => {
     } else {
         menu.classList.add('hidden');
     }
+});
+
+logo.addEventListener('click', () => {
+    window.location.href = 'https://www.instagram.com/cruzard_/';
 });
